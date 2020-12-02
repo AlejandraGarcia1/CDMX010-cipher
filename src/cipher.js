162530ee5513006db3dafe24bb8b410105f96af3
Alejandra García <agarcia_pacheco@hotmail.com>
2020-12-01 20:07:51 -0600

    Interfaz de boleto de loteria

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,32 @@
-const cipher = {
-  // ...
-};
+let getData = function(){
+  let cardName = document.getElementById("name").value
+  cardName.charCodeAt("1")
+  console.log(cardName)
 
-export default cipher;
+}
+
+
+// let cardName = document.getElementById("name").value
+
+//   cardName.charCodeAt(0)
+//   console.log("cardName")
+
+
+// // ------------- EJEMPLO 1 DE MICHELLE ------------
+
+// var cardname = document.getElementById("name");
+
+
+// function cardname.charCodeAt("name"){
+  
+// }
+
+// --------------- oBTENER EL NOMBRE DEL USUARIO Y VERLO EN CONSOLA ----------
+
+// var getData = function (){
+//   var name = document.getElementById("name").value;
+//   string.charCodeAt(name)
+//   console.log(name);
+// }
+
+// export default cipher;
