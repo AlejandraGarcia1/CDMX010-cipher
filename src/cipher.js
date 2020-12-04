536cb41857d3f1e632aa258198073e851e76fb50
Alejandra García <agarcia_pacheco@hotmail.com>
2020-12-04 11:28:41 -0600

    encriptado listo

--- src/cipher.js
+++ src/cipher.js
@@ -1,32 +1,36 @@
-let getData = function(){
-  let cardName = document.getElementById("name").value
-  cardName.charCodeAt("1")
-  console.log(cardName)
+//------ En esta sección se efine la funcionalidad total del proyecto -------
 
-}
+// 1. Se define un objeto, con propiedades y métodos para cifrar y descifrar el código
+const cipher = {
+  // 2. Definimos la funcion para codificar (Propiedad del objeto) con 2 parámetros harcodeados: string y offset
+  encode: function(string, offset) {
+    // let string = "Alejandra"; (parámetro harcodeado)
+    // let offset = 3; (parámetro harcodeado)    
+
+    // 3. Definimos las acciones que se van a ejecutar (Método del objeto) / En este caso implementamos un condicional:
+    if (string){
+      // 4. Definimos un ciclo con el "for" indicando las acciones a ejecutar
+      for (let i = 65; i <= 65; i++);
+      // console.log(string.charCodeAt(offset));
+      // 5. Definimos lo que queremos que nos regrese el ciclo, con el comando de encriptación ".charCodeAt"
+      return string.charCodeAt(offset);      
+      // Nota: El return rompe con la ejecución del for      
+    }    
+  },
+ /*  decode: function() {
+    let number = 65;
+    let offset = 2;
+
+    if (number){
+      for (let i = 65; i <=65; i++ );
+      // console.log(number.fromCharCode(offset));
+
+      return(number.fromCharCode(offset));   
+
+    }
+  } */
+};
 
 
-// let cardName = document.getElementById("name").value
-
-//   cardName.charCodeAt(0)
-//   console.log("cardName")
-
-
-// // ------------- EJEMPLO 1 DE MICHELLE ------------
-
-// var cardname = document.getElementById("name");
-
-
-// function cardname.charCodeAt("name"){
-  
-// }
-
-// --------------- oBTENER EL NOMBRE DEL USUARIO Y VERLO EN CONSOLA ----------
-
-// var getData = function (){
-//   var name = document.getElementById("name").value;
-//   string.charCodeAt(name)
-//   console.log(name);
-// }
-
-// export default cipher;
+//Estamos exportando la página de cipher
+export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+//Importamos la página de cipher
 import cipher from './cipher.js';
 
-console.log(cipher);
+//------ En el index se designa la interacción con el usuario -------
+
+// 1. Definimos una variable para el botón de encriptado
+let buttonEncode = document.getElementById("encode");
+
+// 2. Definimos las acciones que ejecutará el botón al momento de darle "click"
+buttonEncode.onclick = function() {
+    // 3. Definimos las variables que vamos a utilizar y obtenemos su valor
+    let string = document.getElementById('phrase').value;
+    let offset = document.getElementById('space').value;
+    // 4. Definimos una nueva variable que ejecutará el cifrado de la información que ingrese el usuario en los inputs
+    let message = cipher.encode(string, offset);
+    // 5. Lo mostramos en consola para asegurarnos que se esta realizando la acción
+    console.log('MESSAGE', message);
+
+    // 6. Definimos que se debe visualizar la información que obtuvimos de la variable, en la sección de boleto de lotería
+    document.getElementById("encriptNumber").innerHTML = message;  
+} 
+    
+      
+
+
+//Estamos indicando que estamos entrando al objeto de cipher en el parametro de encode y encode
+console.log(cipher.encode());
+//console.log(cipher.decode());
