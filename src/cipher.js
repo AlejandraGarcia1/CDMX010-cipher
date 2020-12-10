56ddb420ddb117c2595e6254bbca297c96fb9d36
Alejandra García <agarcia_pacheco@hotmail.com>
2020-12-10 11:14:25 -0600

    Interfaz y funcionalidad terminada.

--- src/cipher.js
+++ src/cipher.js
@@ -1,36 +1,48 @@
 //------ En esta sección se efine la funcionalidad total del proyecto -------
 
-// 1. Se define un objeto, con propiedades y métodos para cifrar y descifrar el código
-const cipher = {
-  // 2. Definimos la funcion para codificar (Propiedad del objeto) con 2 parámetros harcodeados: string y offset
-  encode: function(string, offset) {
-    // let string = "Alejandra"; (parámetro harcodeado)
-    // let offset = 3; (parámetro harcodeado)    
+const cipher = { //Se crea un objeto con propiedades y métodos para el cifrado / descifrado.  
+  encode: function(userInput, offset){ //Se define la función (propiedad del objeto) con 2 parámetros: string y offset.
+    
+      let code = ""; //Se define una variable donde se guardarán las nuevas letras ("frase encriptada")
+          
+      for (let i=0; i<userInput.length; i++){ //Se define un "For" para indicar el ciclo que se realizará: La función va recorriendo cada letra del userInput hasta llegar a la ultima.       
+       let text = userInput.charCodeAt(i); //Se pasa cada letra del userInput a código ASCII y se guarda en la nueva variable de "texto"
+      
+        if (text >= 97 && text <=122){ //Definimos condicionales. Con el comparador logico "&&" decimos que "text" comience en 65 y no pase de 90.        
+        code += String.fromCharCode((text - 97 + offset) %26 +97); //Se aplica la formúla para obtener el nuevo cifrado. En este caso son "Minusculas".
+        
+       } else if (text >= 65 && text <=90){        
+        code += String.fromCharCode((text - 65 + offset) %26 +65); //Se aplica la formula para obtener el cifrado. En este caso son "Mayusculas".
+        console.log('CIFRADO1', code);        
 
-    // 3. Definimos las acciones que se van a ejecutar (Método del objeto) / En este caso implementamos un condicional:
-    if (string){
-      // 4. Definimos un ciclo con el "for" indicando las acciones a ejecutar
-      for (let i = 65; i <= 65; i++);
-      // console.log(string.charCodeAt(offset));
-      // 5. Definimos lo que queremos que nos regrese el ciclo, con el comando de encriptación ".charCodeAt"
-      return string.charCodeAt(offset);      
-      // Nota: El return rompe con la ejecución del for      
-    }    
+       } else {         
+         code += userInput.charAt(i); //Aplicamos "charAt" para convertir los números directamente a un String.
+         
+       }       
+     }     
+     return code; //Pedimos que nos regrese el resultado de la variable "Code"
   },
- /*  decode: function() {
-    let number = 65;
-    let offset = 2;
+  decode: function (letters, offset){ //Se define el descifrado.
 
-    if (number){
-      for (let i = 65; i <=65; i++ );
-      // console.log(number.fromCharCode(offset));
+    let newOffset = 26 - (offset % 26); //Definimos un nuevo "Offset" para que al desencriptar comience el ciclo desde el prncipio y no retroceda. 
+    let message = "";
 
-      return(number.fromCharCode(offset));   
+    for(let i=0; i<letters.length; i++){
 
+      let text = letters.charCodeAt(i);  
+
+      if (text >= 97 && text <= 122){
+        message += String.fromCharCode((text - 97 + newOffset) %26 +97); //Es la misma formula del encriptado. Lo punico que tiene que cambiar es el "Offset" para desencriptarlo.
+
+      } else if (text >= 65 && text <= 90){
+        message += String.fromCharCode((text - 65 + newOffset) %26 +65);
+
+      } else {
+        message += text.charAt(i);
+      }
     }
-  } */
+    return message;
+  }   
 };
 
-
-//Estamos exportando la página de cipher
-export default cipher;
+export default cipher; //Estamos exportando la página de "Cipher".
--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
-//Importamos la página de cipher
-import cipher from './cipher.js';
+import cipher from './cipher.js'; //Importamos la página de "Cipher".
 
 //------ En el index se designa la interacción con el usuario -------
 
-// 1. Definimos una variable para el botón de encriptado
-let buttonEncode = document.getElementById("encode");
+let buttonEncode = document.getElementById("encode"); //Definimos una variable para el botón de encriptado.
 
-// 2. Definimos las acciones que ejecutará el botón al momento de darle "click"
-buttonEncode.onclick = function() {
-    // 3. Definimos las variables que vamos a utilizar y obtenemos su valor
-    let string = document.getElementById('phrase').value;
+buttonEncode.onclick = function() { //Definimos las acciones que ejecutará el botón al darle "Click".
+    
+    let userInput = document.getElementById('phrase').value; //Definimos las variables que vamos a utilizar y obtenemos su valor.
+    let desplazamiento = document.getElementById('space').value;
+    let offset = parseInt(desplazamiento);
+   
+    let message = cipher.encode(userInput, offset); //Definimos una nueva variable que ejecutará el cifrado de la información que ingrese el usuario en los inputs.
+   
+    document.getElementById("encriptNumber").innerHTML = message; //Definimos que se debe visualizar la información que obtuvimos de la variable, en la sección de boleto de lotería.  
+}; 
+
+let buttonDecode = document.getElementById("decode");
+
+buttonDecode.onclick = function() {
+
+    let letters = document.getElementById('phrase').value;  
     let offset = document.getElementById('space').value;
-    // 4. Definimos una nueva variable que ejecutará el cifrado de la información que ingrese el usuario en los inputs
-    let message = cipher.encode(string, offset);
-    // 5. Lo mostramos en consola para asegurarnos que se esta realizando la acción
-    console.log('MESSAGE', message);
+   
+    let message = cipher.decode(letters,offset);  
 
-    // 6. Definimos que se debe visualizar la información que obtuvimos de la variable, en la sección de boleto de lotería
-    document.getElementById("encriptNumber").innerHTML = message;  
-} 
+    document.getElementById("encriptNumber").innerHTML = message;
+};
     
       
 
 
-//Estamos indicando que estamos entrando al objeto de cipher en el parametro de encode y encode
+//Estamos indicando que estamos entrando al objeto de cipher en el parametro de "Encode" y "Decode".
 console.log(cipher.encode());
-//console.log(cipher.decode());
+console.log(cipher.decode());
